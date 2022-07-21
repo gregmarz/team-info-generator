12bb1e5df03a9d6014fc5a16140f7454ab0f597b
marzec <gregmarzec02@gmail.com>
2022-07-21 19:37:57 -0400

    added base html, questions

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
-const employeeQ = inquirer.prompt [
-    {
+const questions = [
+  {
+    name: "manName",
+    message: "What is the name of the manager?",
+  },
+  {
+    name: "manID",
+    message: "What is the manager's ID?",
+  },
+  {
+    name: "manEmail",
+    message: "What is the manager's email?",
+  },
+  {
+    name: "manOffNum",
+    message: "What is the manager's office number",
+  },
+];
 
-    },
-]
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    console.log(data);
+  });
+}
+
+init();
