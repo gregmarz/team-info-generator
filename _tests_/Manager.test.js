e1115f2293cacb117d9bf0495db405016a34815b
marzec <gregmarzec02@gmail.com>
2022-07-21 18:31:45 -0400

    finished all tests

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,1 +1,12 @@
 const Manager = require("../lib/Manager");
+
+describe("Test", function () {
+  test("check to see if all info gets parsed properly for manager", function () {
+    const Manny = new Manager("Manny", "0001", "manny@gmail.com", 110);
+
+    expect(Manny.name).toBe("Manny");
+    expect(Manny.id).toBe("0001");
+    expect(Manny.email).toBe("manny@gmail.com");
+    expect(Manny.officenumber).toBe(110);
+  });
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require("inquirer");
+
+const employeeQ = inquirer.prompt [
+    {
+
+    },
+]
